fc16b6d0aed90fc6adf4f41c4eb296ff8770edd4
TelmoSalvado <telmosalvado18@gmail.com>
2019-04-26 15:25:46 +0100

    Adicionar Pagina ver tratamentos

---
+++ app/src/main/java/pt/ipg/aminhasaude/VerTratamentos.java
@@ -0,0 +1,26 @@
+package pt.ipg.aminhasaude;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class VerTratamentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_tratamentos);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
