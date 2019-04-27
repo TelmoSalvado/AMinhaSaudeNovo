23a6f1b23336e956acfbb19bc8d8296a175c72d3
TelmoSalvado <telmosalvado18@gmail.com>
2019-04-27 14:35:48 +0100

    Adicionada a pagina Eliminar Consulta!

---
+++ app/src/main/java/pt/ipg/aminhasaude/EliminarConsulta.java
@@ -0,0 +1,34 @@
+package pt.ipg.aminhasaude;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Toast;
+
+public class EliminarConsulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_consulta);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public void Eliminar(View view){
+        Toast.makeText(this, getString(R.string.Eliminar), Toast.LENGTH_SHORT).show();
+        finish();
+    }
+    public void Cancelar(View view){
+        Toast.makeText(this, getString(R.string.Cancelar), Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
